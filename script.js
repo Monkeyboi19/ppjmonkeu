3c2e6ad7be3f19a55dbd7683fa337ca4f4604671
Monkeyboi19 <skyauki2010@gmail.com>
2023-07-14 11:05:41 -0500

    updated

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const panels = document .querySelectorAll(".panel");
+const panels = document.querySelectorAll(".panel");
 
-panels .forEach(panel => {
-    panel.addEventListener("click," () => {
-        removeActiveclasses ()
-        panel.classlist.add("active");
-    }) 
+panels.forEach(panel => {
+    panel.addEventListener("click", () => {
+        removeActiveClasses()
+        panel.classList.add("active");
+    })
 })
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove("active")
+    })
+}
